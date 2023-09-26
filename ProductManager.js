f487c76a4770166e01a065a5ab4d21a92fc5b052
jp-quevedo <quevedo.jpg@gmail.com>
2023-09-26 19:49:24 -0300

    clase 4

---
+++ ProductManager.js
@@ -0,0 +1,90 @@
+import fs from 'fs';
+
+class ProductsManager{
+
+    constructor(path){
+        this.path = path
+        if(!fs.existsSync(this.path)){
+            fs.writeFileSync(this.path,JSON.stringify([]))
+        }
+    }
+
+    async getProducts(queryObj){
+        const { limit } = queryObj
+        try {
+            if(fs.existsSync(this.path)){
+                const info = await fs.promises.readFile(this.path,'utf-8')
+                const parsedInfo = JSON.parse(info)
+                return limit === "5"
+                    ? parsedInfo.slice(0,0+5)
+                    : parsedInfo
+            } else {
+                return []
+            }
+        } catch (error) {
+            return error
+        }
+    }
+
+    async createProduct(obj){
+        try {
+            const products = await this.getProducts()
+            let id
+            if(products.length === 0){
+                id = 1
+            } else {
+                id = products[products.length-1].id+1
+            }
+            const newProduct = {id,...obj}
+            products.push(newProduct)
+            await fs.promises.writeFile(this.path,JSON.stringify(products))
+            return newProduct
+        } catch (error) {
+            return error
+        }
+    }
+
+    async getProductById(productId){
+        try {
+            const products = await this.getProducts()
+            const product = products.find(p=>p.id === productId)
+            if(product){
+                return product
+            } else {
+                return 'Ese producto no existe'
+            }
+        } catch (error) {
+            return error
+        }
+    }
+
+    async deleteProduct(productId){
+        try {
+            const products = await this.getProducts()
+            const product = products.find(p=>p.id === productId)
+            if(!product){
+                return -1
+            }
+            const newProductsArray = products.filter(product=>product.id !== productId)
+            await fs.promises.writeFile(this.path,JSON.stringify(newProductsArray))
+            return 1
+        } catch (error) {
+            return error
+        }
+    }
+
+    async updateProduct(productId,updatedProduct){
+        try {
+            const products = await this.getProducts()
+            const product = products.find(product=>product.id === productId)
+            Object.assign(product,updatedProduct)
+            const newProducts = products.map(p=>p.id == productId?product:p)
+            await fs.promises.writeFile(this.path,JSON.stringify(newProducts))
+        } catch (error) {
+            return error
+        }
+    }
+
+}
+
+export const productsManager = new ProductsManager('Products.json')
---
+++ index.js
@@ -0,0 +1,8 @@
+import http from 'http';
+
+const server = http.createServer()
+
+server.listen(8080,()=>{
+    console.log('Escuchando al puerto 8080')
+})
+

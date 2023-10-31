7421843b9a80989ad9a7f2e98c22496c2a0b1e5e
jp-quevedo <quevedo.jpg@gmail.com>
2023-10-31 15:50:46 -0300

    password hash

--- src/router/mongo/mongosession.router.js
+++ src/router/mongo/mongosession.router.js
@@ -1,15 +1,17 @@
 import usersManager from '../../managers/mongo/mongoUsersManager.js'
 import { Router } from 'express'
+import { compareData, hashData } from '../../utils.js'
 
 const router = Router()
 
 router.post('/signup', async(req, res) => {
     const { name, email, password } = req.body
+    const hashedPassword = await hashData(password)
     if (!name || !email || !password) {
         return res.status(400).json({ message: 'Some data is missing' })
     }
     try {
-        const newUser = await usersManager.createOne(req.body)
+        const newUser = await usersManager.createOne({ ...req.body, password: hashedPassword })
         req.user = newUser
         res.redirect('/api/users')
     } catch (error) {
@@ -21,17 +23,22 @@ router.post('/login', async (req, res) => {
     const { loginEmail, loginPassword } = req.body
     const userInDB = await usersManager.findByEmail(loginEmail)
     if (!userInDB) {
-        return res.status(400).json({ message: 'User not found!' })
+        return res.status(400).json({ message: 'Email or password is wrong' })
     }
     try {
-        req.session['email'] = loginEmail
-        req.session['name'] = userInDB.name
-        if (loginEmail === 'adminCoder@coder.com' && loginPassword === 'Cod3r123'){
-            req.session['isAdmin'] = true
+        const comparePassword = await compareData(loginPassword, userInDB.password)
+        if (!comparePassword) {
+            return res.json({ error: 'Email or password is wrong' })
         } else {
-            req.session['isAdmin'] = false
+            req.session['email'] = loginEmail
+            req.session['name'] = userInDB.name
+            if (loginEmail === 'adminCoder@coder.com' && loginPassword === 'Cod3r123'){
+                req.session['isAdmin'] = true
+            } else {
+                req.session['isAdmin'] = false
+            }
+            res.redirect('/api/session')
         }
-        res.redirect('/api/session')
     } catch (error) {
         res.status(500).json({ message: error })
     }
